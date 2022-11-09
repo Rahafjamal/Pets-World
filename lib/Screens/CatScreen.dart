adad25974d6a410dc8eca5effb39dca22922a019
rahafjamal <email_address>
2022-11-09 20:48:26 +0300

    tabs

--- lib/Screens/CatScreen.dart
+++ lib/Screens/CatScreen.dart
@@ -1,6 +1,13 @@
 import 'package:final_project/Logic/animalapi.dart';
+import 'package:final_project/Logic/containerright.dart';
+import 'package:final_project/Logic/containetLeft.dart';
+import 'package:final_project/Screens/DogScreen.dart';
+import 'package:final_project/Screens/drawerScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_html/shims/dart_ui_real.dart';
+
+import '../main.dart';
 
 class CatScreen extends StatefulWidget {
   const CatScreen({super.key});
@@ -9,7 +16,7 @@ class CatScreen extends StatefulWidget {
   State<CatScreen> createState() => _CatScreenState();
 }
 
-class _CatScreenState extends State<CatScreen> {
+class _CatScreenState extends State<CatScreen> with TickerProviderStateMixin {
   List Data = [];
   @override
   void initState() {
@@ -18,7 +25,7 @@ class _CatScreenState extends State<CatScreen> {
     myobject.get().then((value) {
       print(value);
       setState(() {
-        final Data = value;
+        Data = value;
       });
     });
     super.initState();
@@ -26,58 +33,188 @@ class _CatScreenState extends State<CatScreen> {
 
   @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 5, vsync: this);
     return Scaffold(
-      body: ListView.builder(
-        itemCount: Data.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('images/555.jpg'), fit: BoxFit.fill),
-                ),
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black),
+          backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+          elevation: 0,
+          leading: Builder(
+            builder: (context) => IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: Image.asset(
+                "images/R.png",
+                width: 30,
               ),
-              SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                child: Container(
-                  margin: EdgeInsets.all(5.0),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                      child: Stack(
-                        children: <Widget>[
-                          Image.network(Data[index]["image_link"],
-                              height: 700,
-                              fit: BoxFit.cover,
-                              width: double.infinity),
-                          Positioned(
-                            bottom: 0.0,
-                            left: 0.0,
-                            right: 0.0,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  colors: [
-                                    Color.fromARGB(200, 0, 0, 0),
-                                    Color.fromARGB(0, 0, 0, 0)
-                                  ],
-                                  begin: Alignment.bottomCenter,
-                                  end: Alignment.topCenter,
-                                ),
-                              ),
-                            ),
+            ),
+          ),
+        ),
+        drawer: DrawerScreen(),
+        body: ListView(children: [
+          Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Image.asset(
+                      "images/cool.png",
+                      scale: 4,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 70, top: 100),
+                        child: Text(
+                          "Hello Sweety !",
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
                           ),
-                        ],
-                      )),
-                ),
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ],
-          );
-        },
-      ),
-    );
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 15, top: 5),
+              child: Row(children: const [
+                Text(
+                  "Welcome in Cat Kingdom ",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ])),
+          SizedBox(height: 20),
+          Column(children: [
+            Row(
+              children: const [
+                Text(
+                  " ",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            TabBar(
+              controller: _tabController,
+              tabs: const [
+                Text(
+                  "Tybs",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "Tybs",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "Tybs",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "Tybs",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "Tybs",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+                padding: const EdgeInsets.all(7),
+                width: double.infinity,
+                height: 510,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    Container(
+                        width: double.infinity,
+                        child: CarouselSlider.builder(
+                            options: CarouselOptions(
+                              aspectRatio: 2.0,
+                              enlargeCenterPage: true,
+                              autoPlay: true,
+                            ),
+                            itemCount: Data.length,
+                            itemBuilder: (context, index, realIndex) {
+                              return Container(
+                                width: double.infinity,
+                                child: Container(
+                                  margin: EdgeInsets.all(5.0),
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(5.0)),
+                                      child: Stack(
+                                        children: <Widget>[
+                                          Image.network(
+                                              Data[index]["image_link"],
+                                              height: 700,
+                                              fit: BoxFit.fill,
+                                              width: double.infinity),
+                                          Positioned(
+                                            bottom: 0.0,
+                                            left: 0.0,
+                                            right: 0.0,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                gradient: LinearGradient(
+                                                  colors: [
+                                                    Color.fromARGB(
+                                                        200, 0, 0, 0),
+                                                    Color.fromARGB(0, 0, 0, 0)
+                                                  ],
+                                                  begin: Alignment.bottomCenter,
+                                                  end: Alignment.topCenter,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                ),
+                              );
+                            })),
+                  ],
+                )),
+          ])
+        ]));
   }
 }
