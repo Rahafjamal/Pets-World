a00f110d87e79d8cf6b43bc91e0360c310e678b5
rahafjamal <email_address>
2022-11-09 16:10:03 +0300

    raa

--- lib/Screens/CatScreen.dart
+++ lib/Screens/CatScreen.dart
@@ -1,5 +1,6 @@
 import 'package:final_project/Logic/animalapi.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class CatScreen extends StatefulWidget {
   const CatScreen({super.key});
@@ -26,8 +27,57 @@ class _CatScreenState extends State<CatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Text('rahaf'),
-    ));
+      body: ListView.builder(
+        itemCount: Data.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/555.jpg'), fit: BoxFit.fill),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      child: Stack(
+                        children: <Widget>[
+                          Image.network(Data[index]["image_link"],
+                              height: 700,
+                              fit: BoxFit.cover,
+                              width: double.infinity),
+                          Positioned(
+                            bottom: 0.0,
+                            left: 0.0,
+                            right: 0.0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromARGB(200, 0, 0, 0),
+                                    Color.fromARGB(0, 0, 0, 0)
+                                  ],
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
