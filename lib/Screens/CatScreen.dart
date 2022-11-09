34291124e7ba9f0fc931a607a177a471aeabb04a
rahafjamal <email_address>
2022-11-09 15:30:58 +0300

    ra

--- lib/Screens/CatScreen.dart
+++ lib/Screens/CatScreen.dart
@@ -26,49 +26,8 @@ class _CatScreenState extends State<CatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: Data.length,
-          itemBuilder: (context, index) {
-            return Container(
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(Data[index]["image_link"]),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    "name : " + Data[index]["name"],
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    "length : " + Data[index]["length"],
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    "family_friendly : " +
-                        Data[index]["family_friendly"].toString(),
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  // Text('length $Data[index]["length"]'),
-                  // Text(Data[index]["origin"]),
-                  // Text(Data[index]["family_friendly"]),
-                  // Text(Data[index]["shedding"]),
-                  // Text(Data[index]["general_health"]),
-                  // Text(Data[index]["playfulness"]),
-                  // Text(Data[index]["children_friendly"]),
-                  // Text(Data[index]["grooming"]),
-                  // Text(Data[index]["intelligence"]),
-                  // Text(Data[index]["min_weight"]),
-                  // Text(Data[index]["max_weight"]),
-                  // Text(Data[index]["min_life_expectancy"]),
-                  // Text(Data[index]["max_life_expectancy"]),
-                ],
-              ),
-            );
-          }),
-    );
+        body: Container(
+      child: Text('rahaf'),
+    ));
   }
 }
